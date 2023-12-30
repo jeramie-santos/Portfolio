67a847632e4925fad7e900ebb496e73d12c30f94
Jeramie Santos <jeramieramsantos79@gmail.com>
2023-12-30 16:56:44 +0800

    add show and hide function on nav hamburger and links

---
+++ main.js
@@ -0,0 +1,15 @@
+function show() {
+    document.querySelector('.nav-narrow').style.display = 'block';
+    document.querySelector('.fa-xmark').style.display = 'block';
+    document.querySelector('.fa-bars').style.display = 'none';
+
+
+}
+
+function hide() {
+    document.querySelector('.nav-narrow').style.display = 'none';
+    document.querySelector('.fa-xmark').style.display = 'none';
+    document.querySelector('.fa-bars').style.display = 'block';
+
+}
+
